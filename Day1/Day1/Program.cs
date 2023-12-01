c1cbea71c918227ee05490500ce82124193715a7
Magnumic <64858691+Magnumic0ray@users.noreply.github.com>
2023-12-01 17:10:05 -0500

    D1P1
    it works

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.IO;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+int sum = 0;
+
+try
+{
+    StreamReader sr = new StreamReader("C:\\Users\\Magnumic\\Documents\\GitHub\\AdventOfCode2023\\input\\input.txt");
+    string inputLine = sr.ReadLine();
+    while (inputLine != null)
+    {
+        string output = Regex.Replace(inputLine, @"[^0-9\-]", "");
+        char[] chars = {output[0], output[^1]};
+        sum += Int32.Parse(new string(chars));
+        
+        inputLine = sr.ReadLine();
+    }
+    sr.Close();
+}
+catch(Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+
+Console.WriteLine(sum);
