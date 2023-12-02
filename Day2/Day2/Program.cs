48834a06d2b5408ce841618b4d784ff09721e1a3
Magnumic <64858691+Magnumic0ray@users.noreply.github.com>
2023-12-02 13:10:15 -0500

    D2
    Part 1 Complete

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,31 +1,34 @@
-﻿using System.Collections;
-using System.Text.RegularExpressions;
+﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\input.txt"));
 
 const int redNum = 12;
 const int greenNum = 13;
 const int blueNum = 14;
 
-int redMax = 0;
-int greenMax = 0;
-int blueMax = 0;
+int sum = 0;
 
-try
-{
-    StreamReader sr = new StreamReader("C:\\Users\\Magnumic\\Documents\\GitHub\\AdventOfCode2023\\Day2\\Day2\\input\\" +
-                                       "inputTest.txt");
-    string inputLine = sr.ReadLine();
-    while (inputLine != null)
-    {
+foreach (string inputLine in inputTxt) {
+    bool gamePossible = true;
+    
+    string[] splitGame = inputLine.Split(':');
+    int gameNumber = int.Parse(splitGame[0].Split(' ')[1]);
+    string[] rounds = splitGame[1].Split(';');
+
+    foreach (string round in rounds) {
+        string[] cubes = round.Split(',');
         
-        string filteredLine = Regex.Replace(inputLine, @"[^0-9rgb]", "");
-        Console.WriteLine(filteredLine);
-        //sum += Int32.Parse(charStr);
+        foreach (string cube in cubes) {
+            string[] splitCube = cube.Split(' ');
+            if (splitCube[2] == "red" && int.Parse(splitCube[1]) > redNum)
+                gamePossible = false;
 
-        inputLine = sr.ReadLine();
+            if (splitCube[2] == "green" && int.Parse(splitCube[1]) > greenNum)
+                gamePossible = false;
+
+            if (splitCube[2] == "blue" && int.Parse(splitCube[1]) > blueNum)
+                gamePossible = false;
+        }
     }
-    sr.Close();
+    if (gamePossible) sum += gameNumber;
 }
-catch(Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
-}
+
+Console.WriteLine(sum);
