01edec7a5dab87eabaf344eeaf9e1eff585e5781
Magnumic <64858691+Magnumic0ray@users.noreply.github.com>
2023-12-02 20:02:49 -0500

    D1 file intake updated

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,38 +1,28 @@
 ﻿using System.Text.RegularExpressions;
 
+List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\input.txt"));
+
 int sum = 0;
 
-try
+foreach(string inputLine in inputTxt)
 {
-    StreamReader sr = new StreamReader("C:\\Users\\Magnumic\\Documents\\GitHub\\AdventOfCode2023\\Day1\\Day1\\input\\input.txt");
-    string inputLine = sr.ReadLine();
-    while (inputLine != null)
-    {
-        string filteredLine = Regex.Replace(inputLine, @"eightwo", "82");
-        filteredLine = Regex.Replace(filteredLine, @"twone", "21");
-        filteredLine = Regex.Replace(filteredLine, @"oneight", "18");
-        filteredLine = Regex.Replace(filteredLine, @"one", "1");
-        filteredLine = Regex.Replace(filteredLine, @"two", "2");
-        filteredLine = Regex.Replace(filteredLine, @"three", "3");
-        filteredLine = Regex.Replace(filteredLine, @"four", "4");
-        filteredLine = Regex.Replace(filteredLine, @"five", "5");
-        filteredLine = Regex.Replace(filteredLine, @"six", "6");
-        filteredLine = Regex.Replace(filteredLine, @"seven", "7");
-        filteredLine = Regex.Replace(filteredLine, @"eight", "8");
-        filteredLine = Regex.Replace(filteredLine, @"nine", "9");
-        filteredLine = Regex.Replace(filteredLine, @"[^0-9]", "");
-        char[] chars = {filteredLine[0], filteredLine[^1]};
-        string charStr = new string(chars);
-        //Console.WriteLine(charStr);
-        sum += Int32.Parse(charStr);
-        
-        inputLine = sr.ReadLine();
-    }
-    sr.Close();
-}
-catch(Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
+    string filteredLine = Regex.Replace(inputLine, @"eightwo", "82");
+    filteredLine = Regex.Replace(filteredLine, @"twone", "21");
+    filteredLine = Regex.Replace(filteredLine, @"oneight", "18");
+    filteredLine = Regex.Replace(filteredLine, @"one", "1");
+    filteredLine = Regex.Replace(filteredLine, @"two", "2");
+    filteredLine = Regex.Replace(filteredLine, @"three", "3");
+    filteredLine = Regex.Replace(filteredLine, @"four", "4");
+    filteredLine = Regex.Replace(filteredLine, @"five", "5");
+    filteredLine = Regex.Replace(filteredLine, @"six", "6");
+    filteredLine = Regex.Replace(filteredLine, @"seven", "7");
+    filteredLine = Regex.Replace(filteredLine, @"eight", "8");
+    filteredLine = Regex.Replace(filteredLine, @"nine", "9");
+    filteredLine = Regex.Replace(filteredLine, @"[^0-9]", "");
+    char[] chars = {filteredLine[0], filteredLine[^1]};
+    string charStr = new string(chars);
+    //Console.WriteLine(charStr);
+    sum += Int32.Parse(charStr);
 }
 
 Console.WriteLine(sum);
