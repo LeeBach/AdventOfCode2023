a221a8efb30f00a3dd38c76eb155f70fa7a6fb4f
Magnumic <magnumic0ray@gmail.com>
2023-12-05 14:20:46 -0500

    D4
    Part 2 Complete

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -1,26 +1,17 @@
-﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\inputTest.txt"));
+﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\input.txt"));
 
 int sum = 0;
 int[] cardCopies = new int[inputTxt.Count];
-Console.WriteLine(inputTxt.Count);
-cardCopies[0] = 1;
+Console.WriteLine("# of Cards: {0}", inputTxt.Count);
 
 int index = 0;
 foreach (string card in inputTxt) {
+    cardCopies[index] += 1;
     string[] cardSplit = card.Split(':', '|');
     int matches = 0;
-    
-    string thing = cardSplit[1];
-    List<string> winList = new List<string>();
-    for (int i = 1; i < thing.Length - 1; i += 3)
-        winList.Add(string.Join("", new char[] {thing[i], thing[i + 1]}));
-    string[] win = winList.ToArray();
-    
-    thing = cardSplit[2];
-    List<string> numList = new List<string>();
-    for (int i = 1; i < thing.Length; i += 3)
-        numList.Add(string.Join("", new char[] {thing[i], thing[i + 1]}));
-    string[] nums = numList.ToArray();
+
+    string[] win = ParseToArray(cardSplit[1]);
+    string[] nums = ParseToArray(cardSplit[2]);
         
     bool hasMatches = win.Any(string.Join("", nums).Contains);
     if (hasMatches) {
@@ -33,13 +24,26 @@ foreach (string card in inputTxt) {
         }
     }
     sum += (int)Math.Pow(2, matches - 1);
+    //Console.WriteLine("# of Matches:{0}", matches);
 
-    /*for (int i = index; i < cardCopies[index]; i++) {
-        for (int j = 0; j < matches; j++) {
-            cardCopies[index + j] += 1;
-        }
-    }*/
+    //Console.WriteLine("# of copies of card @ index[{0}]: {1}", index, cardCopies[index]);
+    //Console.WriteLine("----");
+
+    for (int i = 1; i <= matches; i++) {
+        cardCopies[index + i] += cardCopies[index];
+    }
+    index++;
 }
 
 Console.WriteLine("Part 1: {0}", sum);
-//Console.WriteLine("Part 2: {0}", cardCopies.Sum());
+Console.WriteLine("Part 2: {0}", cardCopies.Sum());
+return;
+
+
+
+string[] ParseToArray(string thing) {
+    List<string> list = new List<string>();
+    for (int i = 1; i < thing.Length - 1; i += 3)
+        list.Add(string.Join("", new char[] {thing[i], thing[i + 1]}));
+    return list.ToArray();
+}
