2be573f6e4ae3467fae02586b859bcecfbd635e8
Magnumic <64858691+Magnumic0ray@users.noreply.github.com>
2023-12-04 21:33:37 -0500

    D3

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -1,43 +1,56 @@
-﻿using System.Data;
+﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\inputTest.txt"));
 
-List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\input.txt"));
+int sum = 0;
 
-for (int row = 0; row < inputTxt.Count; row++) {
-    for (int col = 0; col < inputTxt[row].Length; col++) {
+for (int row = 2; row < inputTxt.Count; row++) {
+    for (int col = 2; col < inputTxt[row].Length; col++) {
         if (!char.IsLetterOrDigit(inputTxt[row][col])) {
             int rowAbove = row - 1;
             int rowBelow = row + 1;
             int colLeft = col - 1;
             int colRight = col + 1;
 
-            if (char.IsLetterOrDigit(inputTxt[rowAbove][colLeft]))
-                sum += getNum(rowAbove, colLeft);
+            if (char.IsDigit(inputTxt[rowAbove][colLeft]))
+                Console.Write(inputTxt[rowAbove][colLeft]);
+                sum += GetNum(rowAbove, colLeft);
+            /*if (char.IsDigit(inputTxt[rowAbove][col]))
+                sum += GetNum(rowAbove, col);
+            if (char.IsDigit(inputTxt[rowAbove][colRight]))
+                sum += GetNum(rowAbove, colRight);
 
-            /*
-            if (char.IsLetterOrDigit(inputTxt[rowAbove][col]))
-            if (char.IsLetterOrDigit(inputTxt[rowAbove][colRight]))
+            if (char.IsDigit(inputTxt[row][colLeft]))
+                sum += GetNum(row, colLeft);
+            if (char.IsDigit(inputTxt[row][colRight]))
+                sum += GetNum(row, colRight);
                 
-            if (char.IsLetterOrDigit(inputTxt[row][colLeft]))
-            if (char.IsLetterOrDigit(inputTxt[row][colRight]))
-                
-            if (char.IsLetterOrDigit(inputTxt[rowBelow][colLeft]))
-            if (char.IsLetterOrDigit(inputTxt[rowBelow][col]))
-            if (char.IsLetterOrDigit(inputTxt[rowBelow][colRight]))
-            */
+            if (char.IsDigit(inputTxt[rowBelow][colLeft]))
+                sum += GetNum(rowBelow, colLeft);
+            if (char.IsDigit(inputTxt[rowBelow][col]))
+                sum += GetNum(rowBelow, col);
+            if (char.IsDigit(inputTxt[rowBelow][colRight]))
+                sum += GetNum(rowBelow, colRight);*/
         }
     }
 }
 
-int getNum(int row, int col) {
-    int digits = 0;
-    List<char> numChars = new List<char> {};
+Console.Write(sum);
+
+/*Console.Write(inputTxt[1][3]);
+Console.Write(!char.IsLetterOrDigit(inputTxt[1][3]));*/
+/*List<char> numChars = new List<char> {'1','1','4'};
+Console.Write(int.Parse(string.Join("", numChars)));*/
+
+return;
+
+int GetNum(int row, int col) {
+    List<char> numChars = new List<char>();
     while (char.IsDigit(inputTxt[row][col])) {
         col--;
     }
     while (char.IsDigit(inputTxt[row][col])) {
         numChars.Add(inputTxt[row][col]);
-        digits++;
         col++;
     }
-    return Int32.Parse("".join(numChars));
+    Console.Write(numChars);
+    int.Parse(string.Join("", numChars));
 }
