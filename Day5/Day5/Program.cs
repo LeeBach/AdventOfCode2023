14e4db9ddc1a23dfbcce9bfad6049643827a4476
Magnumic <magnumic0ray@gmail.com>
2023-12-05 15:12:22 -0500

    P5
    tests to try to understand the problem

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\inputTest.txt"));
+
+string seeds = "79 14 55 13";
+string[] seedsArray = seeds.Split(' ');
+
+int map(int value, int destinationRange, int sourceRange, int rangeLength) {
+    if (value >= sourceRange && value <= sourceRange + rangeLength) {
+        return destinationRange + value - sourceRange;
+    }
+    return value;
+}
+
+foreach (string seed in seedsArray) {
+    if (map(int.Parse(seed), 50, 98, 2) != int.Parse(seed))
+        Console.WriteLine(map(int.Parse(seed), 50, 98, 2));
+    else
+        Console.WriteLine(int.Parse(seed));
+    
+    if (map(int.Parse(seed), 52, 50, 48) != int.Parse(seed))
+        Console.WriteLine(map(int.Parse(seed), 52, 50, 48));
+    else
+        Console.WriteLine(int.Parse(seed));
+}
