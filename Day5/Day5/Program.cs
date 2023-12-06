79a72a69dccdf6e3bf422e38a6d6ba1db718ca9e
Magnumic <64858691+Magnumic0ray@users.noreply.github.com>
2023-12-05 19:23:45 -0500

    D5
    Figuring out how do select the right map for each seed

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -1,23 +1,78 @@
-﻿List<string> inputTxt = new(File.ReadAllLines(@"..\..\..\input\inputTest.txt"));
+﻿using System.Text;
 
-string seeds = "79 14 55 13";
-string[] seedsArray = seeds.Split(' ');
+string inputTxt = File.ReadAllText(@"..\..\..\input\inputTest.txt");
 
-int map(int value, int destinationRange, int sourceRange, int rangeLength) {
+/*
+string[] delimitingStrings = {
+    "seed-to-soil map:",
+    "soil-to-fertilizer map:",
+    "fertilizer-to-water map:",
+    "water-to-light map:",
+    "light-to-temperature map:",
+    "temperature-to-humidity map:",
+    "humidity-to-location map:",
+};
+
+string[] splitInput = inputTxt.Split(delimitingStrings, StringSplitOptions.None);
+
+List<string> seeds = new List<string>();
+foreach (string s in splitInput[0].Split(':')[1].Split(new char[] {'\r', '\n', ' '}, StringSplitOptions.RemoveEmptyEntries))
+    seeds.Add(s);
+
+List<string> maps = new List<string>();
+for (int i = 1; i <= 7; i++) {
+    string[] mapSetArray = splitInput[i].Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+    string mapSetString = "";
+    foreach (string s in mapSetArray) {
+        maps.Add(s);
+    }
+}
+
+foreach (string seed in seeds) {
+    int value = int.Parse(seed);
+    foreach (string map in maps) {
+        string[] mapSplit = map.Split(' ');
+        value = Map(value, int.Parse(mapSplit[0]), int.Parse(mapSplit[1]), int.Parse(mapSplit[2]));
+    }
+    Console.WriteLine(value);
+}
+
+Console.WriteLine("*");
+
+return;
+*/
+
+int Map(int value, int destinationRange, int sourceRange, int rangeLength) {
     if (value >= sourceRange && value <= sourceRange + rangeLength) {
         return destinationRange + value - sourceRange;
     }
     return value;
 }
 
-foreach (string seed in seedsArray) {
-    if (map(int.Parse(seed), 50, 98, 2) != int.Parse(seed))
-        Console.WriteLine(map(int.Parse(seed), 50, 98, 2));
-    else
-        Console.WriteLine(int.Parse(seed));
-    
-    if (map(int.Parse(seed), 52, 50, 48) != int.Parse(seed))
-        Console.WriteLine(map(int.Parse(seed), 52, 50, 48));
-    else
-        Console.WriteLine(int.Parse(seed));
-}
+int thing = 55;
+thing = Map(thing, 50, 98, 2);
+thing = Map(thing, 52, 50, 48);
+
+thing = Map(thing, 0, 15, 37);
+thing = Map(thing, 37, 52, 2);
+thing = Map(thing, 39, 0, 15);
+
+thing = Map(thing, 49, 53, 8);
+thing = Map(thing, 0, 11, 42);
+thing = Map(thing, 42, 0, 7);
+thing = Map(thing, 57, 7, 4);
+
+thing = Map(thing, 88, 18, 7);
+thing = Map(thing, 18, 25, 70);
+
+thing = Map(thing, 45, 77, 23);
+thing = Map(thing, 81, 45, 19);
+thing = Map(thing, 68, 64, 13);
+
+thing = Map(thing, 0, 69, 1);
+thing = Map(thing, 1, 0, 69);
+
+thing = Map(thing, 60, 56, 37);
+thing = Map(thing, 56, 93, 4);
+
+Console.WriteLine(thing);
